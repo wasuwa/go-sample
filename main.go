9be59d2d32bf693bf3475d73ee2e686039f2a649
suwa <baru.web.production@gmail.com>
2021-11-03 16:53:24 +0900

    アプリ終了時にdatabaseを閉じる

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"twitter-app/config"
+	"twitter-app/database"
 	"twitter-app/server"
 )
 
 func main() {
 	config.Init("development")
 	server.Init()
+	defer database.Close()
 }
