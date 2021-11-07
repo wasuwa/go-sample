d7cdc27523af5b1a5e1a9897165e3f72114ad5cf
suwa <baru.web.production@gmail.com>
2021-11-07 16:46:03 +0900

    user{}のpasswordは6文字以下で登録できないようにする

--- models/user.go
+++ models/user.go
@@ -9,7 +9,7 @@ type User struct {
 	Id       int    `json:"id"`
 	Name     string `json:"name" validate:"required"`
 	Email    string `json:"email" validate:"required"`
-	Password string `json:"password" validate:"required"`
+	Password string `json:"password" validate:"required,min=6"`
 }
 
 func (u *User) All() ([]User, error) {
