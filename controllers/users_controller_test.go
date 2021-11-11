572c5ad748fae7953ee2ea8590c3e2c0ac8bb952
suwa <baru.web.production@gmail.com>
2021-11-11 20:19:34 +0900

    テストでは、テスト用のDBを参照する

--- controllers/users_controller_test.go
+++ controllers/users_controller_test.go
@@ -18,15 +18,17 @@ var userJSON = `{"name":"mokou","email:"mokou@mokou.com","mokomoko"}`
 func TestIndexUser(t *testing.T) {
 	config.Init("../config/environments/", "test")
 	database.Init()
+	defer database.Close()
 	e := server.Router()
 
 	req := httptest.NewRequest(http.MethodGet, "/users", nil)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
+
 	if assert.NoError(t, controllers.IndexUser(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		// mock DB を用意する必要がある
-		// println(rec.Body.String())
+		println(rec.Body.String())
 	}
 }
