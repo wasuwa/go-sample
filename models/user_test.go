2984565d5b9d5e308fe289a91400605b893950aa
suwa <baru.web.production@gmail.com>
2021-11-16 08:58:47 +0900

    gormで生成される時刻のタイムゾーンをJSTにする

--- models/user_test.go
+++ models/user_test.go
@@ -2,8 +2,33 @@ package models_test
 
 import (
 	"testing"
+	"time"
+	"twitter-app/database"
+	"twitter-app/models"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	t	time.Time
+	user = &models.User{
+		ID:        0,
+		Name:      "takada",
+		Email:     "god@example.com",
+		Password:  "kenshi",
+		CreatedAt: t,
+		UpdatedAt: t,
+	}
 )
 
 func TestAll(t *testing.T) {
+	assert := assert.New(t)
+	db, teardown := database.SetupTestDB()
+	defer teardown()
 
+	db.Create(user)
+	uu, err := user.All()
+
+	assert.Contains(uu[0], user)
+	assert.NoError(err)
 }
