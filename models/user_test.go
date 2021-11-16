fe455e5d0c524e5561d095f0f68a4468b68d1783
suwa <baru.web.production@gmail.com>
2021-11-17 08:49:22 +0900

    user.Createのテストをtabledriventestで書き直す

--- models/user_test.go
+++ models/user_test.go
@@ -19,6 +19,36 @@ var (
 		CreatedAt: t,
 		UpdatedAt: t,
 	}
+	testcases = []struct {
+		name    string
+		input   *models.User
+		wantErr bool
+	}{
+		{
+			"正しく通ること",
+			&models.User{
+				ID:        0,
+				Name:      "takada",
+				Email:     "god@example.com",
+				Password:  "kenshi",
+				CreatedAt: t,
+				UpdatedAt: t,
+			},
+			false,
+		},
+		{
+			"emailの重複でエラーが発生すること",
+			&models.User{
+				ID:        0,
+				Name:      "mokou",
+				Email:     "god@example.com",
+				Password:  "yutaka",
+				CreatedAt: t,
+				UpdatedAt: t,
+			},
+			true,
+		},
+	}
 )
 
 func TestAll(t *testing.T) {
@@ -42,9 +72,17 @@ func TestCreate(t *testing.T) {
 		c int64
 		i int64 = 1
 	)
-	err := user.Create()
-	db.Find(&user).Count(&c)
-
-	assert.Equal(i, c)
-	assert.NoError(err)
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := user.Create()
+			db.Find(&user).Count(&c)
+			if tc.wantErr {
+				assert.NotEqual(i, c)
+				assert.Error(err)
+			} else {
+				assert.Equal(i, c)
+				assert.NoError(err)
+			}
+		})
+	}
 }
