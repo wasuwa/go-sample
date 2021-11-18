7d3e107518353abcab6e3d888b6c974fa3ee94eb
suwa <baru.web.production@gmail.com>
2021-11-18 23:04:31 +0900

    services層にmodelsのFind()を移行する

--- services/todo_service.go
+++ services/todo_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"errors"
 	"twitter-app/database"
 	"twitter-app/models"
 )
@@ -10,4 +11,18 @@ func AllUser() ([]models.User, error) {
 	d := database.DB()
 	d = d.Find(&users)
 	return users, d.Error
+}
+
+func FindUser(id int) (*models.User, error) {
+	u := new(models.User)
+	db := database.DB()
+	db = db.Where("id = ?", id).Find(u)
+	if db.RowsAffected == 0 {
+		return nil, notFoundError()
+	}
+	return u, db.Error
+}
+
+func notFoundError() error {
+	return errors.New("record not found")
 }
