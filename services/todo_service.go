cf878bf59da7140b930f1e1870ffd93d46b85788
suwa <baru.web.production@gmail.com>
2021-11-19 05:38:54 +0900

    services層にCreateUser()を移行する

--- services/todo_service.go
+++ services/todo_service.go
@@ -4,23 +4,51 @@ import (
 	"errors"
 	"twitter-app/database"
 	"twitter-app/models"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func AllUser() ([]models.User, error) {
-	var users []models.User
 	d := database.DB()
+	var users []models.User
 	d = d.Find(&users)
 	return users, d.Error
 }
 
 func FindUser(id int) (*models.User, error) {
-	u := new(models.User)
 	db := database.DB()
+	u := new(models.User)
 	db = db.Where("id = ?", id).Find(u)
 	if db.RowsAffected == 0 {
 		return nil, notFoundError()
 	}
 	return u, db.Error
+}
+
+func CreateUser(ru *models.ReceiveUser) (*models.User, error) {
+	db := database.DB()
+	u := new(models.User)
+	if ru.Password != "" {
+		var err error
+		ru.Password, err = hashPassword(ru.Password)
+		if err != nil {
+			return nil, err
+		}
+	}
+	bindUser(u, ru)
+	db = db.Create(u)
+	return u, db.Error
+}
+
+func hashPassword(pass string) (string, error) {
+	h, err := bcrypt.GenerateFromPassword([]byte(pass), 12)
+	return string(h), err
+}
+
+func bindUser(u *models.User, ru *models.ReceiveUser) {
+	u.Name 		 = ru.Name
+	u.Email 	 = ru.Email
+	u.Password = ru.Password
 }
 
 func notFoundError() error {
