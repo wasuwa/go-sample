114e9156e0a159c45d0fbdf48214f8a7fb81f967
suwa <baru.web.production@gmail.com>
2021-11-20 16:33:03 +0900

    gorm.Modelをbase構造体に移行する

--- models/user.go
+++ models/user.go
@@ -1,12 +1,10 @@
 package models
 
-import "gorm.io/gorm"
-
 type User struct {
-	gorm.Model
-	Name     string `json:"name"`
-	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"password"`
+	Base
+	Name      string    `json:"name"`
+	Email     string    `json:"email" gorm:"unique"`
+	Password  string    `json:"password"`
 }
 
 type ReceiveUser struct {
