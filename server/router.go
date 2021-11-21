1286186dae6bd3a273cddc3d717f4571cb26c453
suwa <baru.web.production@gmail.com>
2021-11-21 14:32:44 +0900

    serverとrouterを反対にする

--- server/router.go
+++ server/router.go
@@ -1,6 +1,30 @@
 package server
 
-func Init() {
-	e := Router()
-	e.Logger.Fatal(e.Start(":8080"))
+import (
+	"twitter-app/controllers"
+	mw "twitter-app/middleware"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func Router() (e *echo.Echo) {
+	e = echo.New()
+
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.SecureWithConfig(middleware.SecureConfig{
+		ContentTypeNosniff: "application/json",
+	}))
+
+	e.Validator = mw.NewValidator()
+
+	e.GET("/users", controllers.IndexUser)
+	e.GET("/users/:id", controllers.ShowUser)
+	e.POST("/users", controllers.CreateUser)
+	e.PATCH("/users/:id", controllers.UpdateUser)
+	e.DELETE("/users/:id", controllers.DestroyUser)
+
+	return
 }
