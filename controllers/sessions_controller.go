e7944b3b09168143e03ab08a6298a27fe03ed7d7
suwa <baru.web.production@gmail.com>
2021-11-21 15:30:06 +0900

    a

--- controllers/sessions_controller.go
+++ controllers/sessions_controller.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
+	"errors"
 	"net/http"
 	"twitter-app/models"
 	"twitter-app/services"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
 
 func Login(c echo.Context) error {
@@ -13,11 +15,13 @@ func Login(c echo.Context) error {
 	if err := c.Bind(ru); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
-	if err := services.SearchUser(ru); err == nil {
-		return nil
-	} else {
+	u, err := services.SearchUser(ru)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return echo.NewHTTPError(http.StatusNotFound, err.Error())
+	} else if u.Password != ru.Password {
+		return echo.NewHTTPError(http.StatusBadRequest, errors.New("password is incorrect"))
 	}
+	return nil
 }
 
 func Logout(c echo.Context) error {
--- services/session_service.go
+++ services/session_service.go
@@ -3,14 +3,18 @@ package services
 import (
 	"twitter-app/database"
 	"twitter-app/models"
+
+	"gorm.io/gorm"
 )
 
 func SearchUser(ru *models.ReceiveUser) (*models.User, error) {
 	db := database.DB()
 	u := new(models.User)
-	db = db.Where("email = ?", ru.Email).Take(u)
+	db = db.Where("email = ?", ru.Email).Find(u)
 	if db.Error != nil {
 		return nil, db.Error
+	} else if db.RowsAffected == 0 {
+		return nil, gorm.ErrRecordNotFound
 	}
 	return u, nil
 }
