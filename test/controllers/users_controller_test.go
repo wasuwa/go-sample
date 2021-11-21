65142c2cc72ee1fbca19fa3cf8fdd19e1bdf81ba
suwa <baru.web.production@gmail.com>
2021-11-21 12:36:26 +0900

    TestCreateUserの修正

--- test/controllers/users_controller_test.go
+++ test/controllers/users_controller_test.go
@@ -1,26 +1,22 @@
 package controllers_test
 
 import (
-	// "fmt"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"time"
-
 	// "strconv"
-
-	// "strings"
+	"strings"
 
 	"testing"
 	"twitter-app/config"
 	"twitter-app/controllers"
 	"twitter-app/database"
-
 	"twitter-app/models"
 	"twitter-app/server"
 
-	// "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
-	// "gorm.io/gorm"
 )
 
 var (
@@ -35,52 +31,52 @@ var (
 		Email:    "god@example.com",
 		Password: "kenshi",
 	}
-	// testcases = []struct {
-	// 	name    string
-	// 	input   string
-	// 	wantErr bool
-	// }{
-	// 	{
-	// 		"正しく通ること",
-	// 		`{"name":"mokou","email":"mokou@example.com","password":"orange"}`,
-	// 		false,
-	// 	},
-	// 	{
-	// 		"パスワードが6文字以上の制限でエラーが返ること",
-	// 		`{"name":"mokou","email":"mokou@example.com","password":"apple"}`,
-	// 		true,
-	// 	},
-	// 	{
-	// 		"名前は15文字以下の制限でエラーが返ること",
-	// 		fmt.Sprintf(`{"name":"%s","email":"mokou@example.com","password":"orange"}`, strings.Repeat("mokou", 4)),
-	// 		true,
-	// 	},
-	// 	{
-	// 		"メールアドレスは256文字以下の制限でエラーが返ること",
-	// 		fmt.Sprintf(`{"name":"mokou","email":"%s","password":"orange"}`, strings.Repeat("mokou", 49)+"@example.com"),
-	// 		true,
-	// 	},
-	// 	{
-	// 		"メールアドレスのフォーマットでエラーが返ること",
-	// 		`{"name":"mokou","email":"examplecom","password":"orange"}`,
-	// 		true,
-	// 	},
-	// 	{
-	// 		"名前は必須の制限でエラーが返ること",
-	// 		`{"email":"mokou@example.com","password":"orange"}`,
-	// 		true,
-	// 	},
-	// 	{
-	// 		"メールアドレスは必須の制限でエラーが返ること",
-	// 		`{"name":"mokou","password":"orange"}`,
-	// 		true,
-	// 	},
-	// 	{
-	// 		"パスワードは必須の制限でエラーが返ること",
-	// 		`{"name":"mokou","email":"mokou@example.com"}`,
-	// 		true,
-	// 	},
-	// }
+	testcases = []struct {
+		name    string
+		input   string
+		wantErr bool
+	}{
+		{
+			"正しく通ること",
+			`{"name":"mokou","email":"mokou@example.com","password":"orange"}`,
+			false,
+		},
+		{
+			"パスワードが6文字以上の制限でエラーが返ること",
+			`{"name":"mokou","email":"mokou@example.com","password":"apple"}`,
+			true,
+		},
+		{
+			"名前は15文字以下の制限でエラーが返ること",
+			fmt.Sprintf(`{"name":"%s","email":"mokou@example.com","password":"orange"}`, strings.Repeat("mokou", 4)),
+			true,
+		},
+		{
+			"メールアドレスは256文字以下の制限でエラーが返ること",
+			fmt.Sprintf(`{"name":"mokou","email":"%s","password":"orange"}`, strings.Repeat("mokou", 49)+"@example.com"),
+			true,
+		},
+		{
+			"メールアドレスのフォーマットでエラーが返ること",
+			`{"name":"mokou","email":"examplecom","password":"orange"}`,
+			true,
+		},
+		{
+			"名前は必須の制限でエラーが返ること",
+			`{"email":"mokou@example.com","password":"orange"}`,
+			true,
+		},
+		{
+			"メールアドレスは必須の制限でエラーが返ること",
+			`{"name":"mokou","password":"orange"}`,
+			true,
+		},
+		{
+			"パスワードは必須の制限でエラーが返ること",
+			`{"name":"mokou","email":"mokou@example.com"}`,
+			true,
+		},
+	}
 )
 
 func TestIndexUser(t *testing.T) {
@@ -100,27 +96,27 @@ func TestIndexUser(t *testing.T) {
 	assert.Equal(http.StatusOK, rec.Code)
 }
 
-// func TestCreateUser(t *testing.T) {
-// 	assert := assert.New(t)
-// 	_, teardown := database.SetupTestDB()
-// 	defer teardown()
+func TestCreateUser(t *testing.T) {
+	assert := assert.New(t)
+	_, teardown := database.SetupTestDB()
+	defer teardown()
 
-// 	e := server.Router()
-// 	for _, tc := range testcases {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			req := httptest.NewRequest(http.MethodPost, "/users", strings.NewReader(tc.input))
-// 			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-// 			rec := httptest.NewRecorder()
-// 			c := e.NewContext(req, rec)
-// 			if err := controllers.CreateUser(c); tc.wantErr {
-// 				assert.Error(err)
-// 			} else {
-// 				assert.NoError(err)
-// 				assert.Equal(http.StatusCreated, rec.Code)
-// 			}
-// 		})
-// 	}
-// }
+	e := server.Router()
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodPost, "/users", strings.NewReader(tc.input))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			if err := controllers.CreateUser(c); tc.wantErr {
+				assert.Error(err)
+			} else {
+				assert.NoError(err)
+				assert.Equal(http.StatusCreated, rec.Code)
+			}
+		})
+	}
+}
 
 // func TestShowUser(t *testing.T) {
 // 	assert := assert.New(t)
